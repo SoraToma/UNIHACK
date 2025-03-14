99db456f275588beea8208043abd1992cc7ae9dd
Guitarband <preetishchoudhary@gmail.com>
2025-03-14 18:51:23 +1100

    Initial Commit

---
+++ webpage/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	content: [
+	  "./index.html",
+	  "./src/**/*.{js,ts,jsx,tsx}",  // Ensuring TypeScript and JSX/TSX files are included
+	],
+	theme: {
+	  extend: {},
+	},
+	plugins: [],
+  }
+  
