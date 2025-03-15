58756bc35c923b3983c7701fd3a0ed4dade3f5b4
Preetish Choudhary <preetishchoudhary@gmail.com>
2025-03-16 01:32:04 +1100

    Revert "Added Auth Pages, set up supabase for user login"

--- webpage/src/components/Toolbar.tsx
+++ webpage/src/components/Toolbar.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-export const Toolbar = ({selected, searchActive, isLight} : {selected : string, searchActive : boolean, isLight: boolean}) => {
+export const Toolbar = ({selected, searchActive} : {selected : string, searchActive : boolean}) => {
 	const [searchQuery, setSearchQuery] = useState("")
 	
 		const searchGame = async () => {
@@ -11,20 +11,20 @@ export const Toolbar = ({selected, searchActive, isLight} : {selected : string, 
 		}
 
 	return (
-		<div className={`flex flex-col items-center ${isLight ? 'bg-[#F4EDED] text-[#0D0D0D]' : 'bg-[#0D0D0D] text-[#F4EDED]'}`}>
+		<div className="flex flex-col items-center">
 			<div className="flex flex-row items-center justify-center p-6 gap-x-16 bg-transparent">
-				<button onClick={() => {window.location.href = '/'}} className="text-2xl font-bold">GameDB</button>
+				<button onClick={() => {}} className="text-2xl">GameDB</button>
 				<button onClick={() => {}} className={`mt-1 ${selected === "Popular Games" ? "text-blue-500" : ""}`}>Popular Games</button>
 				{ searchActive ? 
 				<>
-					<input onChange={(e) => {setSearchQuery(e.target.value)}} onKeyDown={(e) => {if(e.key === "Enter") searchGame()}} value={searchQuery} className={`w-90 h-1/2 p-1 border-b-2 ${isLight ? 'border-[#0D0D0D]' : 'border-[#F4EDED]'}`}  placeholder="Search"></input>
+					<input onChange={(e) => {setSearchQuery(e.target.value)}} onKeyDown={(e) => {if(e.key === "Enter") searchGame()}} value={searchQuery} className="w-90 h-1/2 p-1 border-b-2 border-black" placeholder="Search"></input>
 					<button className="mt-1 mr-10">Sign-in</button>
 				</>
 				:  
 				<>
 					<button onClick={() => {}} className={`mt-1 ${selected === "New Games" ? "text-blue-500" : ""}`}>New Games</button>
 					<button onClick={() => {}} className={`mt-1 ${selected === "Upcoming Games" ? "text-blue-500" : ""}`}>Upcoming Games</button>
-					<button onClick={() => window.location.href = '/auth' } className="mt-1 ml-40 mr-10">Sign-in</button>
+					<button onClick={() => {window.location.href = "/auth"}} className="mt-1 ml-40 mr-10">Sign-in</button>
 				</>
 				}
 			</div>
